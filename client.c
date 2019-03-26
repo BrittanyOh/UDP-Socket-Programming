769c42dda5ed7af68a29849e887f6df942bf569b
Brittany Ohalete <bopho@Brittanys-MacBook-Pro.local>
2019-03-26 01:04:57 -0400

    Check command line for arguments

--- client.c
+++ client.c
@@ -11,7 +11,14 @@
 #define MAXLINE 1000
 
 int main(int argc, char *argv[]){
-  int sockfd;
+  int sockfd, port_num;
+  char *ipaddr;
   char buffer[MAXLINE];
   struct sockaddr_in servaddr;
+
+  //Check commandline for correct arguments
+  if (argc !=3){
+    fprintf(stderr, "REQUIRED FORMAT: %s <server IP> <server port>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 }
